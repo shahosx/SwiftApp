761fb25df9c98a4292263d66e780c829bf5a0fb7
Shahadat Hossain <shahadathossain@shahadats-mbp.home>
2017-01-12 21:26:14 -0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Shahadat Hossain on 1/12/17.
+//  Copyright © 2017 Shahadat Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
